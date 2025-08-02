5132fab0109922b0c4f6c4f8130af1733f02cf46
Jiman Kim <iojjdwn@gmail.com>
2025-08-02 15:24:27 +0900

    register commit

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -16,12 +16,38 @@ export const loginWithGoogle = createAsyncThunk(
 
 export const logout = () => (dispatch) => {};
 export const registerUser = createAsyncThunk(
-  "user/registerUser",
+  // 비동기 처리
+  "user/registerUser", // Redux 내부에서 액션을 식별할 때 쓰는 고유 문자열 ID로  Redux에서 "이건 userSlice의 registerUser 액션이다"라는 걸 나타내는 식별자
   async (
-    { email, name, password, navigate },
-    { dispatch, rejectWithValue }
-  ) => {}
+    { email, name, password, navigate }, // 첫 번째 인자 -> 우리가 넘겨준 payload (회원가입 데이터)
+    { dispatch, rejectWithValue } // 두 번째 인자 → Redux Toolkit의 ThunkAPI (dispatch, rejectWithValue 등)  상태 관리와 에러 처리를 위한 함수
+  ) => {
+    try {
+      const response = await api.post("/user", { email, name, password });
+      // 성공했을 때
+      // 1.성공 토스트 메세지 보여주기
+      dispatch(
+        showToastMessage({ message: "회원가입 성공!", status: "success" })
+      );
+      // 2.로그인 페이지로 리다이렉트
+      navigate("/login");
+      return response.data.data; // axios는 기본적으로 response.data에 데이터를 넣고 + backend에서도 data에 데이터를 넣었으니 data.data
+    } catch (error) {
+      // 실패했을 때
+      // 1.실패 토스트 메세지 보여주기
+      dispatch(
+        showToastMessage({ message: "회원가입 실패!", status: "error" })
+      );
+      // 2.에러값 저장하기
+      return rejectWithValue(error.error);
+    }
+  }
 );
+// dispatch의 역할
+// 두 번째 인자에 dispatch가 있는 이유는 이 Thunk 함수 안에서 여러 액션을 연속 실행할 수 있도록 하기 위해서
+// 회원가입 → 다른 상태 초기화 → 토스트 메시지 → 페이지 이동 등 여러 작업을 한 번에 처리할 때를 위해서 dispatch가 필요
+// rejectWithValue의 역할
+// createAsyncThunk에서 실패했을 때 에러를 전달하는 전용 함수
 
 export const loginWithToken = createAsyncThunk(
   "user/loginWithToken",
@@ -43,7 +69,18 @@ const userSlice = createSlice({
       state.registrationError = null;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => { // extrareducers은 외부에서 발생한 액션, 특히 비동기 액션을 처리하는 전용 영역, 서버 통신 후 Redux Store에 결과를 저장하는 역할
+    builder.addCase(registerUser.pending, (state) => { // 서버와의 통신을 기다릴 때
+              state.loading = true;
+           })
+           .addCase(registerUser.fulfilled, (state) => { // 서버와의 통신이 성공일 때
+              state.loading = false;
+              state.registrationError = null;
+           })
+           .addCase(registerUser.rejected, (state, action) => { // 서버와의 통신이 실패일 때
+              state.registrationError = action.payload
+           })
+  },
 });
 export const { clearErrors } = userSlice.actions;
 export default userSlice.reducer;
--- src/page/RegisterPage/RegisterPage.js
+++ src/page/RegisterPage/RegisterPage.js
@@ -20,11 +20,12 @@ const RegisterPage = () => {
   const [passwordError, setPasswordError] = useState("");
   const [policyError, setPolicyError] = useState(false);
   const { registrationError } = useSelector((state) => state.user);
+  // Redux Store에 전역 상태로 관리되고, 이 값은 서버와의 회원가입 요청 중 실패했을 때 생기는 에러 메시지를 저장
 
-  const register = (event) => {
+  const register = (event) => { // Form의 onSubmit 이벤트 객체를 받는다
     event.preventDefault();
     const { name, email, password, confirmPassword, policy } = formData;
-    const checkConfirmPassword = password === confirmPassword;
+    const checkConfirmPassword = password === confirmPassword; // password와 confirmPassword가 같은지 비교
     if (!checkConfirmPassword) {
       setPasswordError("비밀번호 중복확인이 일치하지 않습니다.");
       return;
@@ -36,18 +37,27 @@ const RegisterPage = () => {
     setPasswordError("");
     setPolicyError(false);
     dispatch(registerUser({ name, email, password, navigate }));
+    // Redux의 dispatch()로 registerUser라는 액션을 실행, registerUser는 createAsyncThunk로 만들어진 비동기 함수이며 서버에 회원가입 요청을 보내고 결과를 Redux 상태에 반영하는 역할
+    // name, email, password, navigate => registerUser로 보내는 인수
   };
 
   const handleChange = (event) => {
     event.preventDefault();
     let { id, value, type, checked } = event.target;
     if (id === "confirmPassword" && passwordError) setPasswordError("");
+    // 비밀번호 확인란이고, 기존에 에러 메시지가 있을 때: 회원가입 버튼을 눌렀는데 재확인이 틀려서 passwordError가 발생했고 -> 비밀번호 확인 칸을 다시 수정하기 시작했을 때 에러 메시지를 지워라
     if (type === "checkbox") {
       if (policyError) setPolicyError(false);
+      // 체크박스 에러는 회원가입 버튼을 눌러야(register) 생기고,
+      // 그 이후 사용자가 체크박스를 눌렀을 때(handleChange) 해제가 된다
       setFormData((prevState) => ({ ...prevState, [id]: checked }));
-    } else {
-      setFormData({ ...formData, [id]: value });
-    }
+      // 둘 다 기존 상태 복사 + 새 값 덮어쓰기. prevState는 React가 최신 상태를 보장하기 위해 콜백으로 전달하는 안전한 패턴.
+      // 체크박스나 빠른 연속 업데이트 같은 비동기 상황에서는 prevState를 쓰는 게 안정적
+      // 핵심은 체크박스이기 때문에 prevState를 쓰는 경우가 많다는 점, 체크박스는 클릭 한 번으로 값이 true ↔ false로 즉시 토글된다 -> 항상 최신 상태를 보장하기 위해 prevState 패턴을 쓰는 것이 안전한 방식
+      // 일반 input(value)도 prevState를 써도 되지만, 순차 입력이라 상태 꼬임 가능성이 적기 때문에 보통 formData를 직접 참조하는 방식을 사용
+    } else {   // else인 경우는 이메일 입력/이름 입력/비밀번호 입력/비밀번호 확인(첫 입력, passwordError 아직 없음)
+      setFormData({ ...formData, [id]: value });     
+    }          // 기존의 formData를 복사 + 입력 받은 값을 키:값 구조로 추가 또는 덮어쓰고 -> 새로운 객체를 만들어 FormData로 저장
   };
 
   return (
@@ -59,7 +69,14 @@ const RegisterPage = () => {
           </Alert>
         </div>
       )}
-      <Form onSubmit={register}>
+      {/* {registrationError && (
+              <div>에러 메시지</div>
+          )}  -> 이게 이제 JSX에서 if문을 간단히 쓴 표현이고 registration 값 즉 서버와의 통신 오류가 있으면 Alert 태그를 렌더링하고, 없으면 안 한다*/} 
+
+
+
+      <Form onSubmit={register}> 
+        {/* 이메일 입력 영역 */}
         <Form.Group className="mb-3">
           <Form.Label>Email</Form.Label>
           <Form.Control
@@ -70,6 +87,10 @@ const RegisterPage = () => {
             required
           />
         </Form.Group>
+
+
+
+        {/* 이름 입력 영역 */}
         <Form.Group className="mb-3">
           <Form.Label>Name</Form.Label>
           <Form.Control
@@ -80,6 +101,10 @@ const RegisterPage = () => {
             required
           />
         </Form.Group>
+
+
+
+        {/* 비밀번호 입력 영역 */}
         <Form.Group className="mb-3">
           <Form.Label>Password</Form.Label>
           <Form.Control
@@ -90,6 +115,10 @@ const RegisterPage = () => {
             required
           />
         </Form.Group>
+
+
+
+        {/* 비밀번호 확인 입력 영역 */}
         <Form.Group className="mb-3">
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
@@ -104,6 +133,10 @@ const RegisterPage = () => {
             {passwordError}
           </Form.Control.Feedback>
         </Form.Group>
+
+
+
+        {/* 이용약관 확인 영역 */}
         <Form.Group className="mb-3">
           <Form.Check
             type="checkbox"
